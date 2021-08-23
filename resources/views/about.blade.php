b162df786705ce29c7814137137d4f3657e2e7d2
masbamm <iqbalyusuffadillah@gmail.com>
2021-08-23 13:02:53 +0700

    Update UI HomePage and Admin

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>About Me</title>
+    </head>
+
+    <body>
+        <h1>Test Doang ini mah</h1>
+    </body>
+
+    </html>
+
+
+
+
+@endsection
